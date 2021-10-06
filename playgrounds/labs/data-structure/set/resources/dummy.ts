aa18654bcaf9a4989c33bdc6cc70c6b305dbc9b8
gitgitWi <wiii@kakao.com>
2021-10-07 00:58:28 +0900

    Feat:Lab:DS:Set: generate dummy of string array

--- playgrounds/labs/data-structure/set/resources/dummy.ts
+++ playgrounds/labs/data-structure/set/resources/dummy.ts
@@ -3,3 +3,6 @@ import {
   DimenseOneObjectDummyGenerator,
   DimenseTwoObjectDummyGenerator,
 } from "../utils";
+
+const stringArrGen = new StringArrayDummyGenerator(100_000, 10_000);
+export const stringArrDummy = stringArrGen.getDummy();
---
+++ playgrounds/labs/data-structure/set/resources/index.ts
@@ -0,0 +1,1 @@
+export { stringArrDummy } from "./dummy";

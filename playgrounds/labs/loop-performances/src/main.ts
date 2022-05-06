a6a56eb6983c21622d0687f034b60799e5c909e6
gitgitWi <wiii@kakao.com>
2022-05-06 00:55:32 +0000

    Feat (loop-perf) test perfs of functions to get min-max

--- playgrounds/labs/loop-performances/src/main.ts
+++ playgrounds/labs/loop-performances/src/main.ts
@@ -1,7 +1,34 @@
 import './style.css';
 
-const app = document.querySelector<HTMLDivElement>('#app')!;
+import { getConsoleTimes } from './utils';
 
-app.innerHTML = `
-  <h1>Test Loop Performances</h1>
-`;
+import { loadAppleMinutes, loadApple30Mins, loadAppleDays } from './dummy';
+import { getMinMaxByLoop, getMinMaxByMath } from './targets';
+
+window.onload = function main() {
+  const app = document.querySelector<HTMLDivElement>('#app')!;
+
+  const Header = document.createElement('h1');
+  Header.innerText = `Test Loop Performances`;
+  app.append(Header);
+
+  [loadAppleDays, loadApple30Mins, loadAppleMinutes].forEach(async (loader) => {
+    await testsWithData(loader);
+  });
+};
+
+async function testsWithData(loader: typeof loadAppleMinutes) {
+  Object.entries(await loader()).map(([key, val]) => {
+    if (!Array.isArray(val)) return;
+
+    getConsoleTimes(() => getMinMaxByLoop(val), getMinMaxByLoop.name, {
+      description: `${key}-${val.length}`,
+    });
+
+    getConsoleTimes(() => getMinMaxByMath(val), getMinMaxByMath.name, {
+      description: `${key}-${val.length}`,
+    });
+  });
+
+  return;
+}

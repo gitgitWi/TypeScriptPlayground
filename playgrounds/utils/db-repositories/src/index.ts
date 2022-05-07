bf8afeb3c5892bbe35ffb4d52097e3618acccac7
gitgitWi <wiii@kakao.com>
2022-05-07 08:31:01 +0000

    Feat (util/db-repo) impl main func for test utils

--- playgrounds/utils/db-repositories/src/index.ts
+++ playgrounds/utils/db-repositories/src/index.ts
@@ -1,1 +1,58 @@
 export * from './repositories';
+export * from './types';
+
+import { SqliteRepository } from './repositories';
+
+// TODO remove after test
+(function main() {
+  const tableName = `stock_prices`;
+  const repo = new SqliteRepository('./test.sqlite');
+
+  repo.createTable({
+    name: tableName,
+    columns: {
+      id: {
+        type: 'INTEGER',
+        isPrimary: true,
+        needAutoIncrement: true,
+      },
+      // 종목명
+      stockName: {
+        type: 'TEXT',
+        isUnique: true,
+        needNotNull: true,
+      },
+      // 주기
+      interval: {
+        type: 'TEXT',
+        needNotNull: true,
+        defaultValue: '1d',
+      },
+      // 차트 시고저종가, 거래량 데이터
+      prices: {
+        type: 'TEXT',
+        needNotNull: true,
+      },
+      // 데이터 시작일
+      periodStart: {
+        type: 'TEXT',
+        needNotNull: true,
+      },
+      // 데이터 마지막일
+      periodEnd: {
+        type: 'TEXT',
+        needNotNull: true,
+      },
+      // api 제공업체
+      vendor: {
+        type: 'TEXT',
+        needNotNull: true,
+      },
+      // 업데이트일, Date.now()
+      updatedAt: {
+        type: 'INTEGER',
+        needNotNull: true,
+      },
+    },
+  });
+})();

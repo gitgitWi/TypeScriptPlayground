d24784670f096fd3842a0982c319828c30c58d35
gitgitWi <wiii@kakao.com>
2022-05-07 00:31:48 +0000

    Feat (loop-perf) test perfs of `Array.sort()`

--- playgrounds/labs/loop-performances/src/main.ts
+++ playgrounds/labs/loop-performances/src/main.ts
@@ -3,7 +3,7 @@ import './style.css';
 import { getConsoleTimes } from './utils';
 
 import { loadAppleMinutes, loadApple30Mins, loadAppleDays } from './dummy';
-import { getMinMaxByLoop, getMinMaxByMath } from './targets';
+import { getMinMaxByLoop, getMinMaxByMath, sortByArraySort } from './targets';
 
 window.onload = function main() {
   const app = document.querySelector<HTMLDivElement>('#app')!;
@@ -13,22 +13,19 @@ window.onload = function main() {
   app.append(Header);
 
   [loadAppleDays, loadApple30Mins, loadAppleMinutes].forEach(async (loader) => {
-    await testsWithData(loader);
+    // await testsWithData(loader, [getMinMaxByLoop, getMinMaxByMath]);
+    await testsWithData(loader, [sortByArraySort]);
   });
 };
 
-async function testsWithData(loader: typeof loadAppleMinutes) {
+async function testsWithData(loader: typeof loadAppleMinutes, funcs: Function[]): Promise<void> {
   Object.entries(await loader()).map(([key, val]) => {
     if (!Array.isArray(val)) return;
 
-    getConsoleTimes(() => getMinMaxByLoop(val), getMinMaxByLoop.name, {
-      description: `${key}-${val.length}`,
-    });
-
-    getConsoleTimes(() => getMinMaxByMath(val), getMinMaxByMath.name, {
-      description: `${key}-${val.length}`,
+    funcs.forEach((func) => {
+      getConsoleTimes(() => func(val), func.name, {
+        description: `${key}-${val.length}`,
+      });
     });
   });
-
-  return;
 }

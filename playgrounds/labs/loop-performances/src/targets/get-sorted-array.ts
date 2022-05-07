ba23db5f15c9c8ad4307b7f389ec794413cea09c
gitgitWi <wiii@kakao.com>
2022-05-07 00:18:19 +0000

    Feat (loop-perf) impl func `sortByArraySort`

--- playgrounds/labs/loop-performances/src/targets/get-sorted-array.ts
+++ playgrounds/labs/loop-performances/src/targets/get-sorted-array.ts
@@ -1,1 +1,8 @@
-export {}
+/**
+ * @see https://v8.dev/blog/array-sort
+ * - JS 엔진마다 내부 알고리즘은 조금씩 다름
+ * - V8 엔진 v7 이후로는 timsort를 사용하는 듯
+ * @see https://stackoverflow.com/questions/57763205/what-is-array-prototype-sort-time-complexity
+ * - Firefox는 merge sort?
+ */
+export const sortByArraySort = (data: number[]) => data.sort((a, b) => a - b);
